82345016c2d0dd19beca962cfc4293da19d21d1c
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-09-13 10:33:16 +0200

    Added Astro db an changed a few css things

---
+++ db/config.ts
@@ -0,0 +1,17 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Event = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    title: column.text(),
+    startDate: column.text(),
+    endDate: column.text(),
+    thumbnailUrl: column.text()
+  }
+});
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {Event}
+});
+//todo add functionality
