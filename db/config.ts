68d9e20402e6c7c80cb7937c4f8f64e73ea26e7f
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-09-15 13:13:51 +0200

    Added a FAB Added a hamburger menü and reworked Header NAv links deleted footer added a theme switch button tried to fix [event] but it doesent work

--- db/config.ts
+++ db/config.ts
@@ -1,17 +1,21 @@
 import { defineDb, defineTable, column } from 'astro:db';
 
-const Event = defineTable({
+const EventTable = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
     title: column.text(),
+    description: column.text(),
     startDate: column.text(),
     endDate: column.text(),
     thumbnailUrl: column.text()
   }
 });
 
+
+
+
 // https://astro.build/db/config
 export default defineDb({
-  tables: {Event}
+  tables: {EventTable},
 });
 //todo add functionality
